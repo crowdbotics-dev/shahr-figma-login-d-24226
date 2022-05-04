4203317069b26ca890f98d653008363085c6c162
Crowdbotics <engineering@crowdbotics.com>
2022-05-04 16:08:08 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Address(models.Model):
+    'Generated Model'
+    user = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="address_user",)
+    city = models.CharField(max_length=256,)
